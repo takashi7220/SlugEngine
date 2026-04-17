537e3b63ab8e35a945a91ff4133584f4d1d8f9c4
nishihamatakashi <nishihamatakashi220@gmail.com>
2026-04-17 19:33:06 +0900

    JsonValueとParserを追加

---
+++ engine/libs/core/include/core/serialize/archiver/archives/json/JsonParser.hpp
@@ -0,0 +1,327 @@
+﻿#pragma once
+
+#include "core/container/String.hpp"
+#include "core/serialize/archiver/archives/json/JsonValue.hpp"
+
+namespace slug::core
+{
+class JsonParser
+{
+public:
+    explicit JsonParser(core::StringView text)
+        : m_text(text)
+        , m_pos(0)
+    {
+    }
+
+    JsonValue Parse()
+    {
+        SkipWS();
+        JsonValue v = ParseValue();
+        SkipWS();
+        if (m_pos != m_text.size())
+        {
+            SLUG_THROW_EXCEPTION("JsonParser: unexpected trailing characters.");
+        }
+        return v;
+    }
+
+private:
+    core::StringView m_text;
+    size_t m_pos;
+
+    void SkipWS()
+    {
+        while (m_pos < m_text.size() && std::isspace(static_cast<unsigned char>(m_text[m_pos])))
+        {
+            ++m_pos;
+        }
+    }
+
+    char Peek() const
+    {
+        if (m_pos >= m_text.size())
+        {
+            SLUG_THROW_EXCEPTION("JsonParser: unexpected end of input.");
+        }
+        return m_text[m_pos];
+    }
+
+    char Get()
+    {
+        if (m_pos >= m_text.size())
+        {
+            SLUG_THROW_EXCEPTION("JsonParser: unexpected end of input.");
+        }
+        return m_text[m_pos++];
+    }
+
+    bool Consume(char c)
+    {
+        if (m_pos < m_text.size() && m_text[m_pos] == c)
+        {
+            ++m_pos;
+            return true;
+        }
+        return false;
+    }
+
+    void Expect(char c)
+    {
+        if (!Consume(c))
+        {
+            core::String str = core::String("JsonParser: expected '") + c + "'.";
+            SLUG_THROW_EXCEPTION(str.c_str());
+        }
+    }
+
+    JsonValue ParseValue()
+    {
+        SkipWS();
+
+        char c = Peek();
+        if (c == '{')
+        {
+            return ParseObject();
+        }
+
+        if (c == '[')
+        {
+            return ParseArray();
+        }
+
+        if (c == '"')
+        {
+            return JsonValue(ParseString());
+        }
+
+        if (c == 't')
+        {
+            return ParseTrue();
+        }
+
+        if (c == 'f')
+        {
+            return ParseFalse();
+        }
+
+        if (c == 'n')
+        {
+            return ParseNull();
+        }
+        if (c == '-' || std::isdigit(static_cast<unsigned char>(c)))
+        {
+            return ParseNumber();
+        }
+
+        SLUG_THROW_EXCEPTION("JsonParser: invalid value.");
+    }
+
+    JsonValue ParseObject()
+    {
+        Expect('{');
+        SkipWS();
+
+        JsonValue::Object obj;
+
+        if (Consume('}'))
+        {
+            return JsonValue(std::move(obj));
+        }
+
+        while (true)
+        {
+            SkipWS();
+            core::String key = ParseString();
+            SkipWS();
+            Expect(':');
+            SkipWS();
+            JsonValue value = ParseValue();
+            obj.emplace(std::move(key), std::move(value));
+            SkipWS();
+
+            if (Consume('}'))
+            {
+                break;
+            }
+            Expect(',');
+            SkipWS();
+        }
+
+        return JsonValue(std::move(obj));
+    }
+
+    JsonValue ParseArray()
+    {
+        Expect('[');
+        SkipWS();
+
+        JsonValue::Array arr;
+
+        if (Consume(']'))
+        {
+            return JsonValue(std::move(arr));
+        }
+
+        while (true)
+        {
+            SkipWS();
+            arr.push_back(ParseValue());
+            SkipWS();
+
+            if (Consume(']'))
+            {
+                break;
+            }
+            Expect(',');
+            SkipWS();
+        }
+
+        return JsonValue(std::move(arr));
+    }
+
+    core::String ParseString()
+    {
+        Expect('"');
+        core::String out;
+
+        while (true)
+        {
+            char c = Get();
+            if (c == '"')
+            {
+                break;
+            }
+
+            if (c == '\\')
+            {
+                char esc = Get();
+                switch (esc)
+                {
+                case '"':
+                    out.push_back('"');
+                    break;
+                case '\\':
+                    out.push_back('\\');
+                    break;
+                case '/':
+                    out.push_back('/');
+                    break;
+                case 'b':
+                    out.push_back('\b');
+                    break;
+                case 'f':
+                    out.push_back('\f');
+                    break;
+                case 'n':
+                    out.push_back('\n');
+                    break;
+                case 'r':
+                    out.push_back('\r');
+                    break;
+                case 't':
+                    out.push_back('\t');
+                    break;
+                default:
+                    SLUG_THROW_EXCEPTION("JsonParser: unsupported escape sequence.");
+                }
+            }
+            else
+            {
+                out.push_back(c);
+            }
+        }
+
+        return out;
+    }
+
+    JsonValue ParseTrue()
+    {
+        Expect('t');
+        Expect('r');
+        Expect('u');
+        Expect('e');
+        return JsonValue(true);
+    }
+
+    JsonValue ParseFalse()
+    {
+        Expect('f');
+        Expect('a');
+        Expect('l');
+        Expect('s');
+        Expect('e');
+        return JsonValue(false);
+    }
+
+    JsonValue ParseNull()
+    {
+        Expect('n');
+        Expect('u');
+        Expect('l');
+        Expect('l');
+        return JsonValue(nullptr);
+    }
+
+    JsonValue ParseNumber()
+    {
+        const size_t start = m_pos;
+
+        if (Consume('-'))
+        {
+        }
+
+        if (!std::isdigit(static_cast<unsigned char>(Peek())))
+        {
+            SLUG_THROW_EXCEPTION("JsonParser: invalid number.");
+        }
+
+        while (m_pos < m_text.size() && std::isdigit(static_cast<unsigned char>(m_text[m_pos])))
+        {
+            ++m_pos;
+        }
+
+        bool isFloat = false;
+
+        if (m_pos < m_text.size() && m_text[m_pos] == '.')
+        {
+            isFloat = true;
+            ++m_pos;
+
+            while (m_pos < m_text.size() && std::isdigit(static_cast<unsigned char>(m_text[m_pos])))
+            {
+                ++m_pos;
+            }
+        }
+
+        if (m_pos < m_text.size() && (m_text[m_pos] == 'e' || m_text[m_pos] == 'E'))
+        {
+            isFloat = true;
+            ++m_pos;
+
+            if (m_pos < m_text.size() && (m_text[m_pos] == '+' || m_text[m_pos] == '-'))
+            {
+                ++m_pos;
+            }
+
+            while (m_pos < m_text.size() && std::isdigit(static_cast<unsigned char>(m_text[m_pos])))
+            {
+                ++m_pos;
+            }
+        }
+
+        const core::String numStr(m_text.substr(start, m_pos - start));
+
+        if (isFloat)
+        {
+            return JsonValue(std::strtod(numStr.c_str(), nullptr));
+        }
+        return JsonValue(static_cast<std::int64_t>(std::strtoll(numStr.c_str(), nullptr, 10)));
+    }
+};
+
+inline JsonValue ParseJson(core::StringView text)
+{
+    return JsonParser(text).Parse();
+}
+}
---
+++ engine/libs/core/include/core/serialize/archiver/archives/json/JsonValue.hpp
@@ -0,0 +1,256 @@
+﻿#pragma once
+
+#include "core/container/UnorderedMap.hpp"
+#include "core/container/Vector.hpp"
+#include "core/container/String.hpp"
+#include <variant>
+
+namespace slug::core
+{
+
+struct JsonValue
+{
+    using Object = core::TUnorderedMap<core::String, JsonValue>;
+    using Array = core::TVector<JsonValue>;
+    using Variant = std::variant<
+        std::nullptr_t,
+        bool,
+        int64_t,
+        double,
+        core::String,
+        Array,
+        Object>;
+
+    Variant value;
+
+    JsonValue()
+        : value(nullptr)
+    {
+    }
+
+    JsonValue(std::nullptr_t)
+        : value(nullptr)
+    {
+    }
+
+    JsonValue(bool v)
+        : value(v)
+    {
+    }
+
+    JsonValue(int64_t v)
+        : value(v)
+    {
+    }
+
+    JsonValue(double v)
+        : value(v)
+    {
+    }
+
+    JsonValue(const core::String& v)
+        : value(v)
+    {
+    }
+
+    JsonValue(core::String&& v)
+        : value(std::move(v))
+    {
+    }
+
+    JsonValue(const char* v)
+        : value(core::String(v))
+    {
+    }
+
+    JsonValue(const Array& v)
+        : value(v)
+    {
+    }
+
+    JsonValue(Array&& v)
+        : value(std::move(v))
+    {
+    }
+
+    JsonValue(const Object& v) : value(v)
+    {
+    }
+
+    JsonValue(Object&& v)
+        : value(std::move(v))
+    {
+    }
+
+    bool IsNull()   const
+    {
+        return std::holds_alternative<std::nullptr_t>(value);
+    }
+
+    bool IsBool()   const
+    {
+        return std::holds_alternative<bool>(value);
+    }
+
+    bool IsInteger()    const
+    {
+        return std::holds_alternative<std::int64_t>(value);
+    }
+    bool IsFloating() const
+    {
+        return std::holds_alternative<double>(value);
+    }
+    bool IsString() const
+    {
+        return std::holds_alternative<core::String>(value);
+    }
+    bool IsArray()  const
+    {
+        return std::holds_alternative<Array>(value);
+    }
+    bool IsObject() const
+    {
+        return std::holds_alternative<Object>(value);
+    }
+
+    bool& AsBool()
+    {
+        return std::get<bool>(value);
+    }
+    const bool& as_bool() const
+    {
+        return std::get<bool>(value);
+    }
+
+    int64_t& AsInteger()
+    {
+        return std::get<std::int64_t>(value);
+    }
+    const int64_t& AsInteger() const
+    {
+        return std::get<std::int64_t>(value);
+    }
+
+    double& AsFloating()
+    {
+        return std::get<double>(value);
+    }
+    const double& AsFloating() const
+    {
+        return std::get<double>(value);
+    }
+
+    core::String& AsString()
+    {
+        return std::get<core::String>(value);
+    }
+
+    const core::String& AsString() const
+    {
+        return std::get<core::String>(value);
+    }
+
+    Array& AsArray()
+    {
+        return std::get<Array>(value);
+    }
+    const Array& AsArray() const
+    {
+        return std::get<Array>(value);
+    }
+
+    Object& AsObject()
+    {
+        return std::get<Object>(value);
+    }
+    const Object& AsObject() const
+    {
+        return std::get<Object>(value);
+    }
+
+    static core::String EscapeString(const core::String& s)
+    {
+        core::String out;
+        out.reserve(s.size());
+
+        for (char c : s)
+        {
+            switch (c)
+            {
+            case '\"': out += "\\\""; break;
+            case '\\': out += "\\\\"; break;
+            case '\b': out += "\\b"; break;
+            case '\f': out += "\\f"; break;
+            case '\n': out += "\\n"; break;
+            case '\r': out += "\\r"; break;
+            case '\t': out += "\\t"; break;
+            default: out += c; break;
+            }
+        }
+
+        return out;
+    }
+
+    core::String ToString() const
+    {
+        if (IsNull())
+        {
+            return "null";
+        }
+
+        if (IsBool())
+        {
+            return as_bool() ? "true" : "false";
+        }
+
+        if (IsInteger())
+        {
+            return core::StringUtility::ToString(AsInteger());
+        }
+
+        if (IsFloating())
+        {
+            core::String str = {};
+            core::StringUtility::Sprintfs(str, "%.17g", AsFloating());
+            return str;
+        }
+
+        if (IsString())
+        {
+            return "\"" + EscapeString(AsString()) + "\"";
+        }
+
+        if (IsArray())
+        {
+            core::String out = "[";
+            bool first = true;
+            for (const auto& e : AsArray())
+            {
+                if (!first)
+                {
+                    out += ",";
+                }
+                first = false;
+                out += e.ToString();
+            }
+            out += "]";
+            return out;
+        }
+
+        core::String out = "{";
+        bool first = true;
+        for (const auto& [k, v] : AsObject())
+        {
+            if (!first)
+            {
+                out += ",";
+            }
+            first = false;
+            out += "\"" + EscapeString(k) + "\":" + v.ToString();
+        }
+        out += "}";
+        return out;
+    }
+};
+
+}
