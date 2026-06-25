42459a732bf6f183adcd5e29b0ea2080362a6103
nishihamatakashi <nishihamatakashi220@gmail.com>
2026-06-25 22:41:34 +0900

    add submodule

---
+++ AddSubmodule.bat
@@ -0,0 +1,6 @@
+git submodule add https://github.com/google/googletest.git engine/thirdparty/googletest
+git submodule add https://github.com/syoyo/tinygltf.git engine/thirdparty/tinygltf
+git submodule add https://github.com/grpc/grpc.git engine/thirdparty/grpc
+git submodule add https://github.com/google/benchmark.git engine/thirdparty/google_benchmark
+git submodule add https://github.com/g-truc/glm.git engine/thirdparty/glm
+git submodule add https://github.com/shader-slang/slang.git engine/thirdparty/slang/source
