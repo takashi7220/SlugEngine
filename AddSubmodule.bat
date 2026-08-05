2d4cd16e38eed324c72cab89f09af10188eadce9
nishihamatakashi <nishihamatakashi220@gmail.com>
2026-08-06 03:38:39 +0900

    thridpartyライブラリのsubmodule先を変更

--- AddSubmodule.bat
+++ AddSubmodule.bat
@@ -1,7 +1,7 @@
-git submodule add https://github.com/google/googletest.git engine/thirdparty/googletest
-git submodule add https://github.com/syoyo/tinygltf.git engine/thirdparty/tinygltf
-git submodule add https://github.com/grpc/grpc.git engine/thirdparty/grpc
-git submodule add https://github.com/google/benchmark.git engine/thirdparty/google_benchmark
-git submodule add https://github.com/g-truc/glm.git engine/thirdparty/glm
+git submodule add https://github.com/google/googletest.git engine/thirdparty/googletest/source
+git submodule add https://github.com/syoyo/tinygltf.git engine/thirdparty/tinygltf/source
+git submodule add https://github.com/grpc/grpc.git engine/thirdparty/grpc/source
+git submodule add https://github.com/google/benchmark.git engine/thirdparty/google_benchmark/source
+git submodule add https://github.com/g-truc/glm.git engine/thirdparty/glm/source
 git submodule add https://github.com/shader-slang/slang.git engine/thirdparty/slang/source
-git submodule add https://github.com/microsoft/mimalloc.git engine/thirdparty/mimalloc
+git submodule add https://github.com/microsoft/mimalloc.git engine/thirdparty/mimalloc/source
